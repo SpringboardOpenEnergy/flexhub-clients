3ed39fdd455bbb60ec37342746a2519361079887
steinarruneeriksen <s.r.eriksen@gmail.com>
2021-10-28 09:10:38 +0200

    Updated samples

--- readings/ingest_mainmeter_readings_random.py
+++ readings/ingest_mainmeter_readings_random.py
@@ -8,16 +8,16 @@ import pytz, random
 # Same as other sample on main meter, just generates a larger number of random values
 def generate_readings():
     reads=[]
-    from_date = (datetime.now() + timedelta(days=-20))
+    from_date = (datetime.now() + timedelta(days=-20))  #20 days back in time and hourly until yesterday
     from_date = from_date.astimezone(pytz.utc).replace(hour=0, minute=0, second=0)
     to_date = (datetime.today() + timedelta(days=-1))
     to_date = to_date.astimezone(pytz.utc).replace(hour=0, minute=0, second=0)
     while from_date<to_date:
-        valu = random.uniform(3472.5, 3492.9)
+        value = random.uniform(3472.5, 3492.9)   #Random between these numbers
         from_date = from_date + timedelta(hours=1)
         reads.append({
               "timestamp": datetime.utcfromtimestamp(from_date.timestamp()).strftime('%Y-%m-%dT%H:%M:%SZ'),
-              "value": "%.2f" % valu
+              "value": "%.2f" % value
             })
     return reads
 
