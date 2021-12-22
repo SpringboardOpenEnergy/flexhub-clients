9aa01dfe8dcd03a91201e195aa18ab6d466c5068
steinarruneeriksen <s.r.eriksen@gmail.com>
2021-12-22 13:59:03 +0100

    Updated samples

---
+++ misc/healthcheck.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from settings.loader import load_env
+import os
+
+def exec_healthcheck(server_url):
+    url= server_url + '/api/healthcheck/'
+    result = requests.get(url)
+    print(result)
+    print(json.dumps(result.json(), indent=2))
+    if len(result.json())==0:
+        return None
+    return result.json()  # For simplicity in sample, select first company - most common case is only 1
+
+if __name__ == '__main__':
+    load_env()
+    server_url = os.environ.get("server_url")
+    exec_healthcheck (server_url)
